9c6b20f3c695584836b09bd6227dedbcb9985907
andrena2003 <75400129+andrena2003@users.noreply.github.com>
2026-07-27 22:30:10 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/andrenaportfolio">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
